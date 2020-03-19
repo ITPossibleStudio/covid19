1fcc451fb465ccdd2dd2a5cbf1c533adb031d6f5
Volodymyr Zaplatynskii <v.zaplatunskuj@crosssoft.de>
2020-03-19 11:19:28 +0200

    7: include input-mat-module

---
+++ src/app/interfaces/country.interface.ts
@@ -0,0 +1,12 @@
+export interface CountryInterface {
+  country_name: string;
+  cases: string;
+  deaths: string;
+  region: string;
+  total_recovered: string;
+  new_cases: string;
+  new_deaths: string;
+  serious_critical: string;
+  active_cases: string;
+  total_cases_per_1m_population: string;
+}
